34808037535e680cacbaaba5bb98a362719b50c9
Phi Truong <Phi.Truong@NakivoVN.local>
2025-11-07 17:21:51 +0700

    Add canonical URL and www to non-www redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig={experimental:{typedRoutes:true}};
+const nextConfig={
+  experimental:{typedRoutes:true},
+  async redirects(){
+    return[
+      {
+        source:'/:path*',
+        has:[{type:'host',value:'www.vietecotex.com'}],
+        destination:'https://vietecotex.com/:path*',
+        permanent:true,
+      },
+    ];
+  },
+};
 export default nextConfig;
